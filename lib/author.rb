9bf71e5a1ed09cfb8cb7dceffa493ddce6246223
Berry  Semexan <bsemexan@gmail.com>
2018-02-20 19:58:40 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -23,8 +23,8 @@ class Author
     @@post_count += 1
   end
 
-  def add_post_by_title(name)
-    post = Post.new(name)
+  def add_post_by_title(title)
+    post = Post.new(title)
     @posts << post
     post.title = self
   end
